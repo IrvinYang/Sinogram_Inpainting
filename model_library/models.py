359ebbf364e97751ba1bf060c396f3e890a4f78d
IrvnYang <you@example.com>
2023-08-28 12:41:19 -0700

    update 08/26

--- model_library/models.py
+++ model_library/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17f5014d57fb0c0fd8ae9a2c89dcdcf8fe1b383f54814f4744d83d483e952092
-size 1951
+oid sha256:b2da6c046c133ae753104e4d728a665667fc6fe0cac76209c877852790fff40a
+size 7216
